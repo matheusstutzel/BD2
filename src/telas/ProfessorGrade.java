fa46140f43408e9a5c0a22972d53a61eaddd15ee
Matheus Stutzel <matheusstutzel@yahoo.com.br>
2015-11-26 16:11:45 -0200

    Thread secundaria para criar grade

--- src/telas/ProfessorGrade.java
+++ src/telas/ProfessorGrade.java
@@ -5,39 +5,52 @@ package telas;
  */
 
 import br.uerj.bd2_2015_2.DBHelper;
+import javafx.concurrent.Task;
 import javafx.scene.control.Label;
 import telas.Super.Grade;
 
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ProfessorGrade extends Grade {
-    public HashMap<String, ArrayList<String[]>> turmaHoraDia = new HashMap<String, ArrayList<String[]>>();
 
-    public void getHorariosByMatricula(String mat) {
-        try {
-            ResultSet rs = DBHelper.getInstance().connection.createStatement().executeQuery("select ma.cod_turma,ma.dia,(ma.inicio-7)as inicio,mat.nome from Ministrar m Join Turma t on(m.cod_turma=t.cod_turma) join Marcado ma on (t.cod_turma=ma.cod_turma) join Materia mat on(t.cod_materia=mat.cod_materia) where m.matricula_professor = " + mat);
-            String cod_turma, nome, dia, inicio;
-            while (rs.next()) {
-                nome = rs.getString("nome");
-                dia = rs.getString("dia");
-                inicio = rs.getString("inicio");
-                cod_turma = rs.getString("cod_turma");
-                if (turmaHoraDia.get(cod_turma) == null)
-                    turmaHoraDia.put(cod_turma, new ArrayList<String[]>());
-                turmaHoraDia.get(cod_turma).add(new String[]{dia, inicio, nome});
-                System.out.println((dia + ";" + inicio + ";" + nome));
+    public void getHorariosByMatricula(final String mat) {
+        new Thread(new Task<HashMap<String, ArrayList<String[]>>>() {
+            @Override
+            protected HashMap<String, ArrayList<String[]>> call() throws Exception {
+                HashMap<String, ArrayList<String[]>> turmaHoraDia = new HashMap<String, ArrayList<String[]>>();
+                ResultSet rs = DBHelper.getInstance().connection.createStatement().executeQuery("select ma.cod_turma,ma.dia,(ma.inicio-7)as inicio,mat.nome from Ministrar m Join Turma t on(m.cod_turma=t.cod_turma) join Marcado ma on (t.cod_turma=ma.cod_turma) join Materia mat on(t.cod_materia=mat.cod_materia) where m.matricula_professor = " + mat);
+                String cod_turma, nome, dia, inicio;
+                while (rs.next()) {
+                    nome = rs.getString("nome");
+                    dia = rs.getString("dia");
+                    inicio = rs.getString("inicio");
+                    cod_turma = rs.getString("cod_turma");
+                    if (turmaHoraDia.get(cod_turma) == null)
+                        turmaHoraDia.put(cod_turma, new ArrayList<String[]>());
+                    turmaHoraDia.get(cod_turma).add(new String[]{dia, inicio, nome});
+                    System.out.println((dia + ";" + inicio + ";" + nome));
+                }
+                return turmaHoraDia;
             }
-            for (String key : turmaHoraDia.keySet()) {
-                for (String[] v : turmaHoraDia.get(key)) {
-                    gridPane.add(new Label(key + "\n" + v[2]), Integer.parseInt(v[0]), Integer.parseInt(v[1]));
+
+            @Override
+            protected void succeeded() {
+                super.succeeded();
+                HashMap<String, ArrayList<String[]>> turmaHoraDia = null;
+                try {
+                    turmaHoraDia = get();
+                    for (String key : turmaHoraDia.keySet()) {
+                        for (String[] v : turmaHoraDia.get(key)) {
+                            gridPane.add(new Label(key + "\n" + v[2]), Integer.parseInt(v[0]), Integer.parseInt(v[1]));
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        }).start();
     }
 }
 
