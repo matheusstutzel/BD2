09a304f1e0e2d974964d20562ee62a8c4e69ae66
Michel <michelpf13@gmail.com>
2015-11-26 01:38:39 -0200

    grade preenchida(professor)

--- src/telas/Teste.java
+++ src/telas/Teste.java
@@ -227,6 +227,27 @@ public class Teste {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+        } else if (nomeTabela.getText().equals("Professor") && table.getSelectionModel().getSelectedCells().size() == 1) {
+            String aux = null;
+            for (Object o : table.getSelectionModel().getSelectedCells()) {
+                aux = ((ObservableList) t.get(((TablePosition) o).getRow())).get(0).toString();
+            }
+            Stage stage = new Stage();
+            FXMLLoader root = null;
+            try {
+                root = new FXMLLoader(
+                        Teste.class.getResource("professorGrade.fxml"));
+                stage.setScene(new Scene((Pane) root.load()));
+                stage.setTitle("Grade de horário");
+                stage.initModality(Modality.WINDOW_MODAL);
+                stage.initOwner(botaoMaisOpcoes.getScene().getWindow());
+                ProfessorGrade controller =
+                        root.<ProfessorGrade>getController();
+                controller.initData(aux);
+                stage.show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         } else new Alert(Alert.AlertType.WARNING, "Selecione uma linha da tabela abaixo").show();
     }
 
